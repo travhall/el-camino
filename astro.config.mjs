f14e91cba982c3f9e7a9812a30a8cd1bdc971737
Travis <unkletas@gmail.com>
2024-09-07 22:40:04 -0500

    package and directory updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon({ iconDir: "src/assets/icons" })],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
